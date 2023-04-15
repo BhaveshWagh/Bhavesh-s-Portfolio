767a328e84c72072ab4dd0878ebb2b49f957927e
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-04-15 15:52:35 +0530

    About section is added

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 const About = () => {
   return (
     <div>
-    <Header />
-      About
+      <Header />
+      <ul class="list-non-bullet">
+        <li>
+          <img
+            className="self-img"
+            src="../images/self_img-up.png"
+            alt="hero"
+          />
+          <article class="showcase-list container container-center">
+            <h1>i'm Bhavesh Wagh</h1>
+            <small>17 October 2021</small>
+            <p>
+              I am a final year computer engineering student. I'm interested in
+              full stack development. I have a good knowledge about Python,
+              React.js, JavaScript, & C programming language, SQL & HTML/CSS .
+              I'm looking for exciting opportunity to learn more.
+            </p>
+          </article>
+        </li>
+      </ul>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
