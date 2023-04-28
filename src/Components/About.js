b5f23adef90901dcfe91b30a3e3ba2aafc13ca5a
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-04-28 16:56:04 +0530

    about section updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,26 +5,31 @@ const About = () => {
   return (
     <div>
       <Header />
-      <ul class="list-non-bullet">
-        <li>
-          <img
-            className="self-img"
-            src="../images/self_img-up.png"
-            alt="hero"
-          />
-          <article class="showcase-list container container-center">
-            <h1>I'm Bhavesh Wagh</h1>
-            <small>17 October 2021</small>
-            <p>
-              I am a final year computer engineering student. I'm interested in
-              full stack development. I have a good knowledge about Python,
-              <span className="bold"> React.js, JavaScript,</span> C
-              programming language, SQL & HTML/CSS. I'm looking for exciting
-              opportunity to learn more.
-            </p>
-          </article>
-        </li>
-      </ul>
+
+      <header className="hero">
+        <img className="hero-img" src="../images/self_img-up.png" alt="hero" />
+        <h1 className="hero-heading">
+          Hello I'm
+          <span className="heading-inverted"> Bhavesh Wagh </span>
+        </h1>
+      </header>
+
+
+      <section className="section ow">
+      <div className="container container-center">
+        <h1>Hire Me</h1>
+        <article>
+        <p>
+          I am a final year computer engineering student. I'm interested in
+          full stack development. I have a good knowledge about Python,
+          <span className="bold"> React.js, JavaScript,</span> C programming
+          language, SQL & HTML/CSS. I'm looking for exciting opportunity to
+          learn more.
+        </p>
+      </article>
+      </div>
+    </section>
+
       <Footer />
     </div>
   );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,13 +1,75 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
 const Projects = () => {
   return (
-    <div>
-    <Header/>
-    Projects
-    
-    </div>
-  )
-}
+    <React.Fragment>
+      <Header />
+      <header className="hero">
+        <h1 className="hero-heading">
+          See my <span className="heading-inverted"> code</span>
+        </h1>
 
-export default Projects
+        <ul className="list-non-bullet">
+          <li>
+            <div className="showcase-list container container-center ow">
+              <h1>marvel fans</h1>
+
+              <p>
+                A CLI app built with <strong>nodeJS</strong> nodeJS. You can
+                send this to your friends and find out who really knows you
+              </p>
+              <Link
+                className="link link-primary"
+                target="_blank"
+                rel="noopener noreferrer"
+                to="https://replit.com/@BhaveshWagh/end-game#index.js"
+              >
+                Live Project
+              </Link>
+              <Link
+                className="link link-secondary"
+                target="_blank"
+                rel="noopener noreferrer"
+                to="https://github.com/BhaveshWagh"
+              >
+                View Source
+              </Link>
+            </div>
+          </li>
+          <li>
+            <div className="showcase-list container container-center">
+              <h1>how well do you know me?</h1>
+
+              <p>
+                A CLI app built with <strong>nodeJS</strong> nodeJS. You can
+                send this to your friends and find out who really knows you
+              </p>
+              <Link
+                className="link link-primary"
+                target="_blank"
+                rel="noopener noreferrer"
+                to="https://replit.com/@BhaveshWagh/end-game#index.js"
+              >
+                Live Project
+              </Link>
+              <Link
+                className="link link-secondary"
+                target="_blank"
+                rel="noopener noreferrer"
+                to="https://github.com/BhaveshWagh"
+              >
+                View Source
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </header>
+
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default Projects;
